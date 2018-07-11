250dab3960caf5f8f325fd6a38104865f8ce5b0b
Sheng Daoxu <31882070+AllanSheng@users.noreply.github.com>
2018-07-11 23:21:57 +0800

    catalan

---
+++ catalan.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int combination(int n, int k)
+{
+        if(k>=n || k == 0)
+                return 1;
+        int res = 1, i;
+        for(i = n; i > k; i--)
+                res *= i;
+        for(i = 1; i <= k; i++)
+                res /= i;
+        return res;
+}
+
+int catalan(int n)
+{
+        int m = combination(2*n, n);
+        return m/(n+1);
+}
+
+int main()
+{
+        int N;
+        cout << "Input catalan N: ";
+        cin >> N;
+        cout << catalan(N);
+
+        system("pause");
+}
+
